15a50d8d0b54f819698a8517fe1963ba2e960ec9
Homelander-007 <karamveersinghisking@gmail.com>
2024-07-07 18:04:23 +0530

    dex.js

---
+++ dex.js
@@ -0,0 +1,10 @@
+// window.onload = function() {
+//    var name = prompt("Write your name below :");
+//    if (name === null) {
+//       alert("You did not enter your name.");
+//   } else if (name.trim() === "") {
+//       alert("Welcome, Sir!");
+//   } else {
+//       alert("Hello, " + name + "!");
+//   }
+// }
